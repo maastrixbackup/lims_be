55d1809570fb1e33e0a3d3a83600e0d068f06ac1
pravatmaastrix <pravat.maastrix@gmail.com>
2025-10-10 15:34:57 +0530

    User delete api

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,7 @@ const {
   forgotPassword,
   resetPassword,
   updateUser,
+  deleteUser,
 } = require("../controllers/authController");
 
 const authMiddleware = require("../middleware/authMiddleware");
@@ -15,5 +16,6 @@ router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 router.put("/updateUser/:id", authMiddleware, updateUser);
+router.delete("/deleteUser/:id", authMiddleware, deleteUser);
 
 module.exports = router;
