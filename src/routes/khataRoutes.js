b8dbd64ee0205399e2c1e1ff497f39f7a1e94c76
pravatmaastrix <pravat.maastrix@gmail.com>
2025-10-13 18:16:22 +0530

    addKhata api

--- src/routes/khataRoutes.js
+++ src/routes/khataRoutes.js
@@ -5,14 +5,3 @@ const { addKhata } = require("../controllers/khataController");
 router.post("/addKhata", addKhata);
 
 module.exports = router;
-
-// // List
-// router.get("/khatas", authMiddleware, khataList);
-
-// // Update
-// router.put("/khatas/:id", authMiddleware, updateKhata);
-
-// // Delete
-// router.delete("/khatas/:id", authMiddleware, deleteKhata);
-
-// module.exports = router;
