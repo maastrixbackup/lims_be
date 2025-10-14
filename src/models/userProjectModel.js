8836d6c2be4a5ef37ad8f8f1fa2e567fc255b3e8
pravatmaastrix <pravat.maastrix@gmail.com>
2025-10-14 12:31:25 +0530

    Accessed projects in login api

--- src/models/userProjectModel.js
+++ src/models/userProjectModel.js
@@ -15,12 +15,24 @@ const UserProject = {
   },
 
   async getProjectsByUserId(userId) {
-    const [rows] = await db.query(
-      "SELECT project_id FROM user_projects WHERE user_id = ?",
-      [userId]
-    );
-    return rows.map((row) => row.project_id);
+    const query = `
+      SELECT p.id,p.project_name,p.status
+      FROM user_projects up
+      INNER JOIN projects p ON up.project_id = p.id
+      WHERE up.user_id = ? AND p.status = 1
+      ORDER BY p.project_name ASC
+    `;
+    const [rows] = await db.query(query, [userId]);
+    return rows;
   },
+
+  // async getProjectsByUserId(userId) {
+  //   const [rows] = await db.query(
+  //     "SELECT project_id FROM user_projects WHERE user_id = ?",
+  //     [userId]
+  //   );
+  //   return rows.map((row) => row.project_id);
+  // },
 };
 
 module.exports = UserProject;
