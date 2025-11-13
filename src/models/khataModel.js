8175acfe0b4d296f76201be66c907e188ae85f80
pravatmaastrix <pravat.maastrix@gmail.com>
2025-11-13 18:45:53 +0530

    Khata count added in dashboard data

--- src/models/khataModel.js
+++ src/models/khataModel.js
@@ -151,6 +151,13 @@ const Khata = {
       );
     }
   },
+
+  async countAll() {
+    const [rows] = await db.query(
+      "SELECT COUNT(*) AS total FROM khatas"
+    );
+    return rows[0].total;
+  },
 };
 
 module.exports = Khata;
