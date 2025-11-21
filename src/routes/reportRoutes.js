03857d9f1f0de509227383511ba69f6ecdf878da
pravatmaastrix <pravat.maastrix@gmail.com>
2025-11-21 10:56:52 +0530

    Plot count and tenant count added

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   khataSummary,
   getAllKhataDocuments,
+  getVillageReport,
 } = require("../controllers/reportController");
 
 router.get("/khataSummary", khataSummary);
 router.get("/getAllKhataDocuments", getAllKhataDocuments);
+router.get("/villageLandRegister", getVillageReport);
 
 module.exports = router;
