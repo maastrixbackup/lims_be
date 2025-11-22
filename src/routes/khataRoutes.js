b608b1aa3c338fc258db95034ba3c2a659f8c13a
pravatmaastrix <pravat.maastrix@gmail.com>
2025-11-22 12:47:07 +0530

    env changed

--- src/routes/khataRoutes.js
+++ src/routes/khataRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 const {
   addKhata,
   khataList,
@@ -16,7 +17,29 @@ router.post("/addKhata", addKhata);
 router.get("/khataList", khataList);
 router.put("/updateKhata/:id", updateKhata);
 router.delete("/deleteKhata/:id", deleteKhata);
-router.post("/uploadKhata", uploadKhata.single("file"), uploadKhataDoc);
+
+// router.post("/uploadKhata", uploadKhata.single("file"), uploadKhataDoc);
+router.post(
+  "/uploadKhata",
+  (req, res, next) => {
+    uploadKhata.single("file")(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        return res.status(400).json({
+          success: false,
+          message: err.message,
+        });
+      } else if (err) {
+        return res.status(400).json({
+          success: false,
+          message: "Invalid file. Only PDF or Excel files are allowed.",
+        });
+      }
+      next(); // If no error → go to controller
+    });
+  },
+  uploadKhataDoc
+);
+
 router.get("/getKhataFiles/:id", getKhataFilesByKhataId);
 router.delete("/deleteKhataFile/:id", deleteKhataFileById);
 router.get("/viewPlotsByKhata/:id", viewPlotsByKhata);
