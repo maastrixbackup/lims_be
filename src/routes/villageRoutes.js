914ebe387501679343bbb0e7c1d6acfd6c017ec0
pravatmaastrix <pravat.maastrix@gmail.com>
2025-11-27 16:45:32 +0530

    Village export api

--- src/routes/villageRoutes.js
+++ src/routes/villageRoutes.js
@@ -6,11 +6,13 @@ const {
   villageList,
   updateVillage,
   deleteVillage,
+  exportVillage,
 } = require("../controllers/villageController");
 
 router.post("/addVillage", addVillage);
 router.get("/villageList", villageList);
 router.put("/updateVillage/:id", updateVillage);
 router.delete("/deleteVillage/:id", deleteVillage);
+router.get("/exportVillage", exportVillage);
 
 module.exports = router;
