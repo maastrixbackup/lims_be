cae632a67dd517cec3a62c32da84b8a4d155cadb
pravatmaastrix <pravat.maastrix@gmail.com>
2025-11-29 10:37:23 +0530

    View plots by khata modified

--- src/controllers/khataController.js
+++ src/controllers/khataController.js
@@ -358,11 +358,19 @@ const deleteKhataFileById = async (req, res) => {
 const viewPlotsByKhata = async (req, res) => {
   const userId = req.user.id;
   const khata_id = req.params.id;
+  const { type } = req.query;
   try {
     if (!khata_id) {
       return res.status(400).json({
         success: false,
         message: "Khata ID is required",
+      });
+    }
+
+    if (!type) {
+      return res.status(400).json({
+        success: false,
+        message: "Plot type is required",
       });
     }
 
@@ -375,7 +383,7 @@ const viewPlotsByKhata = async (req, res) => {
     }
     const { khata_no } = khataData;
 
-    const plots = await Plot.findByKhataNo(khata_no);
+    const plots = await Plot.findByKhataNo(khata_no, type);
     return res.status(200).json({
       success: true,
       message: "Plots fetched successfully",
