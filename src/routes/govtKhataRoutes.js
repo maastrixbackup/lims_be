608ab895c63822db49e0b81c2638dcd3685f0c15
pravatmaastrix <pravat.maastrix@gmail.com>
2026-01-15 16:54:15 +0530

    Govt khata update api completed

--- src/routes/govtKhataRoutes.js
+++ src/routes/govtKhataRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   addGovtKhata,
   govtKhataList,
+  updateGovtKhata,
 } = require("../controllers/govtKhataController");
 
 router.post("/addGovtKhata", addGovtKhata);
 router.get("/govtKhataList", govtKhataList);
+router.put("/updateGovtKhata/:id", updateGovtKhata);
 
 module.exports = router;
