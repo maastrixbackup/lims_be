485a65941fca03265e2c86ad678f8e7e482bcb8a
pravatmaastrix <pravat.maastrix@gmail.com>
2026-01-23 15:38:21 +0530

    Forest land schedule delete api completed

--- src/routes/forestLandRoutes.js
+++ src/routes/forestLandRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const { addForestLand, updateForestLand, forestLandList } = require("../controllers/forestLandController");
+const { addForestLand, updateForestLand, forestLandList, deleteForestLand } = require("../controllers/forestLandController");
 
 router.post("/addForestLand", addForestLand);
 router.put("/updateForestLand/:id", updateForestLand);
 router.get("/forestLandList", forestLandList);
+router.delete("/deleteForestLand/:id", deleteForestLand);
 
 module.exports = router;
