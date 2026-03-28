3871f4d6d4e64057e38f5d7755895d5b0e74a9d3
Ritishree-12 <ritishree1996@gmail.com>
2026-03-28 10:54:45 +0530

    done

--- src/routes/forestLandRoutes.js
+++ src/routes/forestLandRoutes.js
@@ -51,6 +51,17 @@ router.get("/forestProjectList", forestProjectList);
 //     uploadEDS.single("eds_document"),
 //     updateForestProject
 // );
+router.post(
+    "/forest-project",
+    uploadEdsDocuments.any(), //for dynamic multiple EDS files
+    addForestProjectWithEds
+);
+router.get("/forestProjectList", forestProjectList);
+router.put(
+    "/updateForestProject/:id",
+    uploadEdsDocuments.single("eds_reply_document"),
+    updateForestProject
+);
 
 router.delete("/deleteForestProject/:id", deleteForestProject);
 
@@ -75,6 +86,27 @@ router.post(
     addStage0
 );
 
+router.put(
+    "/updateStage0/:forest_project_id",
+    uploadStage0.fields([
+        { name: "dgps_document", maxCount: 1 },
+        { name: "orsac_document", maxCount: 1 },
+        { name: "tree_enumeration_document", maxCount: 1 },
+        { name: "administrative_document", maxCount: 1 },
+        { name: "legal_lease_document", maxCount: 1 },
+        { name: "technical_document", maxCount: 1 },
+        { name: "forest_land_details_document", maxCount: 1 },
+        { name: "ca_ca_document", maxCount: 1 },
+        { name: "fra_document", maxCount: 1 },
+        { name: "environmental_document", maxCount: 1 },
+        { name: "wildlife_document", maxCount: 1 },
+        { name: "maps_document", maxCount: 1 },
+        { name: "financial_document", maxCount: 1 },
+        { name: "proposal_document", maxCount: 1 },
+    ]),
+    updateStage0
+);
+
 router.post(
     "/addStage1",
     uploadStage1.fields([
@@ -92,6 +124,23 @@ router.post(
     addStage1
 );
 
+router.put(
+    "/updateStage1/:forest_project_id",
+    uploadStage1.fields([
+        { name: "stage1_approval_document", maxCount: 1 },
+        { name: "stage1_conditions_document", maxCount: 1 },
+        { name: "ca_land_document", maxCount: 1 },
+        { name: "fra_document", maxCount: 1 },
+        { name: "npv_document", maxCount: 1 },
+        { name: "ca_payment_document", maxCount: 1 },
+        { name: "aca_payment_document", maxCount: 1 },
+        { name: "wildlife_document", maxCount: 1 },
+        { name: "technical_document", maxCount: 1 },
+        { name: "stage1_acceptance_document", maxCount: 1 },
+    ]),
+    updateStage1
+);
+
 router.post(
     "/addStage2",
     uploadStage2.fields([
@@ -103,6 +152,19 @@ router.post(
         { name: "stage2_approval_document", maxCount: 1 },
     ]),
     addStage2
+);
+
+router.put(
+    "/updateStage2/:forest_project_id",
+    uploadStage2.fields([
+        { name: "environmental_document", maxCount: 1 },
+        { name: "nbwl_document", maxCount: 1 },
+        { name: "final_ca_document", maxCount: 1 },
+        { name: "final_maps_document", maxCount: 1 },
+        { name: "final_technical_document", maxCount: 1 },
+        { name: "stage2_approval_document", maxCount: 1 },
+    ]),
+    updateStage2
 );
 
 router.post(
@@ -117,9 +179,25 @@ router.post(
     addPostClearance
 );
 
+router.put(
+    "/updatePostClearance/:forest_project_id",
+    uploadPostClearance.fields([
+        { name: "ca_plantation_started_document", maxCount: 1 },
+        { name: "ca_plantation_completed_document", maxCount: 1 },
+        { name: "survival_report_document", maxCount: 1 },
+        { name: "wildlife_mitigation_document", maxCount: 1 },
+        { name: "safety_zone_document", maxCount: 1 },
+    ]),
+    updatePostClearance
+);
+
 // router.get("/getForestProject/:projectId", getForestProjectWithEds);
 router.get("/getStageStatus/:project_id/:stage", getStageStatus);
 
 router.get("/masterDashboardSummary", getMasterDashboard);
+router.get("/getStage0/:forest_project_id", getStage0);
+router.get("/getStage1/:forest_project_id", getStage1);
+router.get("/getStage2/:forest_project_id", getStage2);
+router.get("/getPostClearance/:forest_project_id", getPostClearance);
 
 module.exports = router;
